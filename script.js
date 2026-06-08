07761851a75267c091f571f2e2edc3275c01fece
Sumant9761 <142734516+Sumant9761@users.noreply.github.com>
2026-06-08 22:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
+// Your code here.
 const slider = document.querySelector('.items');
 
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX;
-  scrollLeft = slider.scrollLeft;
+slider.addEventListener('mousedown', function(e) {
+    isDown = true;
+    slider.classList.add('active');
+
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
+slider.addEventListener('mouseleave', function() {
+    isDown = false;
+    slider.classList.remove('active');
 });
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+slider.addEventListener('mouseup', function() {
+    isDown = false;
+    slider.classList.remove('active');
 });
 
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
+slider.addEventListener('mousemove', function(e) {
+    if (!isDown) return;
 
-  e.preventDefault();
+    e.preventDefault();
 
-  const walk = (e.pageX - startX) * 3;
-  slider.scrollLeft = scrollLeft - walk;
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX;
+
+    slider.scrollLeft = scrollLeft - walk;
 });
